fefbe371a44374306defce5a6ce854d29e74e3bd
Judah Kerr <mrjkerr87@gmail.com>
2022-05-18 21:56:44 -0700

    Initial Commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Exercitation sit cillum sit quis aliqua in adipisicing aute. Proident
+        reprehenderit enim deserunt Lorem minim non eu qui. Sit exercitation
+        elit nisi incididunt nulla esse eiusmod aliquip. Quis laboris id duis
+        deserunt amet incididunt ipsum minim do aute ullamco culpa ex duis.
+        Cupidatat minim aliquip est sunt culpa tempor nostrud nostrud. Anim
+        officia veniam ex quis eu elit ad deserunt dolor dolore consectetur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
